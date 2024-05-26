0b572737a879a1d4118271916403460bd07e6eb4
Mohsen Fayyaz <mohsen.fayyaz77@gmail.com>
2024-05-26 00:48:10 -0700

    Implement target_velocity for humanoid

--- src/envs/humanoid.py
+++ src/envs/humanoid.py
@@ -1,10 +1,120 @@
 import numpy as np
-from gymnasium.envs.mujoco.humanoid_v4 import HumanoidEnv, mass_center
+from gymnasium import utils
+from gymnasium.spaces import Box
+from gymnasium.envs.mujoco import MujocoEnv
+from gymnasium.envs.mujoco.humanoid_v4 import HumanoidEnv, mass_center, DEFAULT_CAMERA_CONFIG
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 class CustomHumanoidEnv(HumanoidEnv):
-    def __init__(self, **kwargs,):
-        super().__init__(**kwargs,)
+    def __init__(
+            self,
+            forward_reward_weight=1.25,
+            ctrl_cost_weight=0.1,
+            healthy_reward=5.0,
+            terminate_when_unhealthy=False,
+            healthy_z_range=(0.5, 3.0),
+            reset_noise_scale=5e-2,
+            exclude_current_positions_from_observation=True,
+            **kwargs,
+    ):
+        utils.EzPickle.__init__(
+            self,
+            forward_reward_weight,
+            ctrl_cost_weight,
+            healthy_reward,
+            terminate_when_unhealthy,
+            healthy_z_range,
+            reset_noise_scale,
+            exclude_current_positions_from_observation,
+            **kwargs,
+        )
+
+        # ADDED {
+        self.target_velocity = np.array([-1.0, 1.0])
+        # }
+        self._forward_reward_weight = forward_reward_weight
+        self._ctrl_cost_weight = ctrl_cost_weight
+        self._healthy_reward = healthy_reward
+        self._terminate_when_unhealthy = terminate_when_unhealthy
+        self._healthy_z_range = healthy_z_range
+
+        self._reset_noise_scale = reset_noise_scale
+
+        self._exclude_current_positions_from_observation = (
+            exclude_current_positions_from_observation
+        )
+
+        if exclude_current_positions_from_observation:
+            observation_space = Box(
+                low=-np.inf, high=np.inf,
+                # shape=(376,),
+                shape=(376 + 2,),
+                dtype=np.float64
+            )
+        else:
+            observation_space = Box(
+                low=-np.inf, high=np.inf,
+                # shape=(378,),
+                shape=(378 + 2,),
+                dtype=np.float64
+            )
+
+        MujocoEnv.__init__(
+            self,
+            "humanoid.xml",
+            5,
+            observation_space=observation_space,
+            default_camera_config=DEFAULT_CAMERA_CONFIG,
+            **kwargs,
+        )
+
+    def _get_obs(self):
+        position = self.data.qpos.flat.copy()
+        velocity = self.data.qvel.flat.copy()
+
+        com_inertia = self.data.cinert.flat.copy()
+        com_velocity = self.data.cvel.flat.copy()
+
+        actuator_forces = self.data.qfrc_actuator.flat.copy()
+        external_contact_forces = self.data.cfrc_ext.flat.copy()
+
+        if self._exclude_current_positions_from_observation:
+            position = position[2:]
+
+        return np.concatenate(
+            (
+                position,
+                velocity,
+                com_inertia,
+                com_velocity,
+                actuator_forces,
+                external_contact_forces,
+                # ADDED
+                self.target_velocity,
+            )
+        )
+
+    def reset_model(self):
+        noise_low = -self._reset_noise_scale
+        noise_high = self._reset_noise_scale
+
+        qpos = self.init_qpos + self.np_random.uniform(
+            low=noise_low, high=noise_high, size=self.model.nq
+        )
+        qvel = self.init_qvel + self.np_random.uniform(
+            low=noise_low, high=noise_high, size=self.model.nv
+        )
+        self.set_state(qpos, qvel)
+
+        observation = self._get_obs()
+
+        # ADDED {
+        angle = self.np_random.uniform(0, 2 * np.pi)
+        self.target_velocity = np.array([np.cos(angle), np.sin(angle)])
+        print("target_velocity:", self.target_velocity)
+        # }
+        return observation
 
     def step(self, action):
         xy_position_before = mass_center(self.model, self.data)
@@ -16,8 +126,13 @@ class CustomHumanoidEnv(HumanoidEnv):
 
         ctrl_cost = self.control_cost(action)
 
+        # ADDED {
+        cos_sim = cosine_similarity(xy_velocity.reshape(1, -1), self.target_velocity.reshape(1, -1))[0][0]
+        magnitude_diff = (np.linalg.norm(xy_velocity) - np.linalg.norm(self.target_velocity)) ** 2
+        velocity_reward = 5 * cos_sim + 0.5 * magnitude_diff
+        forward_reward = self._forward_reward_weight * velocity_reward
+        # }
         # forward_reward = self._forward_reward_weight * x_velocity
-        forward_reward = -self._forward_reward_weight * x_velocity
         healthy_reward = self.healthy_reward
 
         rewards = forward_reward + healthy_reward
--- src/mohsen.py
+++ src/mohsen.py
@@ -1,8 +1,8 @@
 from train import train
 from test import test
 
-train(environment_id="CustomHumanoid-v4", saving_interval=100, show_demo=True, output_dir="checkpoints_humanoid/")
-test(environment_id="CustomHumanoid-v4", checkpoint_dir="src/checkpoints_humanoid/0.zip")
+train(environment_id="CustomHumanoid-v4", epochs=20000, saving_interval=100, show_demo=True, output_dir="checkpoints_humanoid/")
+# test(environment_id="CustomHumanoid-v4", checkpoint_dir="checkpoints_humanoid/1000.zip")
 
-train(environment_id="ControlAnt-v4", saving_interval=100, show_demo=True, output_dir="checkpoints_ant/")
-test(environment_id="CustomHumanoid-v4", checkpoint_dir="src/checkpoints_ant/0.zip")
+# train(environment_id="ControlAnt-v4", saving_interval=100, show_demo=True, output_dir="checkpoints_ant/")
+# test(environment_id="CustomHumanoid-v4", checkpoint_dir="src/checkpoints_ant/0.zip")
